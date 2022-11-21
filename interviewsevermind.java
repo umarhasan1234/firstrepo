2182414929d25247a4c983d0e33c2f0152e34790
Umar Hasan <88262838+umarhasan1234@users.noreply.github.com>
2022-11-21 19:16:35 +0530

    first github file
    i am commiting first github file

---
+++ interviewsevermind.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class interviewsevermind {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the row");
+		int row=sc.nextInt();
+		
+		
+		for(int i=0;i<row;i++) {
+			if(i==0) {
+				for(int j=0;j<3;j++) {
+					if(j==1) {
+						System.out.print("1");
+					}
+					else
+					{
+						System.out.print(" ");
+					}
+					
+				}
+				System.out.println();
+				
+			}
+			else {
+				for(int j=0;i<3;j++) {
+					if(j==1) {
+						System.out.print("1"+" ");
+					}
+					else {
+						System.out.print("2"+" ");
+					}
+				}
+				System.out.println();
+			}
+			
+			
+		}
+	}
+}
